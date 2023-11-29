4982830a3744e13615b9310766889557bb9c095d
Austin Purtell <purtellap@gmail.com>
2023-11-29 16:49:47 -0500

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:ps/firebase_options.dart';
 import 'package:ps/portfolio.dart';
 import 'package:ps/res/res.dart';
 import 'package:ps/util/routes.dart';
@@ -9,12 +10,7 @@ import 'home.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //     options: FirebaseOptions(
-  //         apiKey: apiKey,
-  //         appId: appId,
-  //         messagingSenderId: messagingSenderId,
-  //         projectId: projectId));
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
